fe9f84961c2e288c4b962efcc53891de6afac2de
Mark Wagner <mark@quicsystems.com>
2016-04-08 10:52:56 -0500

    Moar renaming

--- reachedge.php
+++ reachedge.php
@@ -1,11 +1,11 @@
 <?php
 ######################################################################
-# ReachLocal Capture                                                 #
+# ReachEdge                                                          #
 # Copyright (C) 2015 by ReachLocal, Inc.                             #
 # Homepage   : www.reachlocal.com                                    #
 # Author     : ReachLocal, Inc.                                      #
 # Email      : support@reachlocal.com                                #
-# Version    : 1.0.4.0                                               #
+# Version    : 1.0.5.0                                               #
 # License    : http://www.gnu.org/copyleft/gpl.html GNU/GPL          #
 ######################################################################
 
@@ -20,27 +20,27 @@ class plgSystemReachLocalTracking extends JPlugin
 	function plgSystemReachLocalTracking(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
-		$this->_plugin = JPluginHelper::getPlugin( 'system', 'reachlocaltracking' );
+		$this->_plugin = JPluginHelper::getPlugin( 'system', 'reachedge' );
 		$this->_params = new JRegistry( $this->_plugin->params );
 	}
 	
 	function onAfterRender()
 	{
 		$mainframe = JFactory::getApplication();
-		$reachlocal_tracking_id = $this->params->get('reachlocal_tracking_id', '');
-		if($reachlocal_tracking_id == '' || $mainframe->isAdmin() || strpos($_SERVER["PHP_SELF"], "index.php") === false)
+		$reachedge_site_id = $this->params->get('reachedge_site_id', '');
+		if($reachedge_site_id == '' || $mainframe->isAdmin() || strpos($_SERVER["PHP_SELF"], "index.php") === false)
 		{
 			return;
 		}
 
 		$buffer = JResponse::getBody();
-		$reachlocaltracking_javascript = "
+		$reachedge_javascript = "
 		<script type='text/javascript'>
-				var rl_siteid = '".$reachlocal_tracking_id."'</script>
+				var rl_siteid = '".$reachedge_site_id."'</script>
 		<script type='text/javascript' src='//cdn.rlets.com/capture_static/mms/mms.js' async='async'></script>		
 		";
 		
-		$buffer = str_replace ("</head>", $reachlocaltracking_javascript."</head>", $buffer);
+		$buffer = str_replace ("</head>", $reachedge_javascript."</head>", $buffer);
 		JResponse::setBody($buffer);
 		return true;
 	}
